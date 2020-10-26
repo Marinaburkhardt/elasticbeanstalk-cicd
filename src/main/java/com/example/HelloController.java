ea0ca9e6e649e7a6c81b8a013ee195597a53bd67
Marinaburkhardt <burkhardtmarina@gmail.com>
2020-10-26 18:39:12 +0100

    Changing html template

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -8,7 +8,7 @@ import io.micronaut.http.annotation.Produces;
 @Controller("/")
 public class HelloController {
 
-    public static final String HELLO_WORLD_BODY_HTML = "<html><body><h1>Hello World!</body></html>";
+    public static final String HELLO_WORLD_BODY_HTML = "<html><body><h1>Hi Elasticbeanstalk friend! <br><h5>CICD implementation</h5></body></html>";
 
     @Get
     @Produces(MediaType.TEXT_HTML)
